16a10e5eef450cb43014bea789f4958b88257236
Abdurauf Sherkulov <abduraufsherkulov@gmail.com>
2019-08-11 23:30:37 +0500

    some changes

--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -14,142 +14,16 @@ function Categories() {
                         <Title className="d-inline-block"><a>Недвижимость</a></Title>
                     </div>
                     <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                    </div>
-                </Col>
-                <Col span={6} className="padding-bottom">
-                    <div className="d-flex-vertical">
-                        <div className="d-inline-block"><img src={Img} /></div>
-                        <Title className="d-inline-block"><a>Недвижимость</a></Title>
-                    </div>
-                    <div className="sub-categories">
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
-                        <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
+                        <ul>
+                            <li>
+                                <Title level={2}><a>Квартиры</a><span>32 216</span></Title>
+                                <ul>
+                                    <li><Title level={2}><a>Аренда долгосрочная</a><span>12 233</span></Title></li>
+                                    <li><Title level={2}><a>Аренда долгосрочная</a><span>12 233</span></Title></li>
+                                    <li><Title level={2}><a>Аренда долгосрочная</a><span>12 233</span></Title></li>
+                                </ul>
+                            </li>
+                        </ul>
                     </div>
                 </Col>
             </Row>
