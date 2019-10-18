693b324525296cc65f781bf2c000d7d34c555d1a
abduraufsherkulov <abduraufsherkulov@gmail.com>
2019-10-18 18:15:04 +0500

    context

---
+++ src/components/context/MyContext.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ThemeContext = React.createContext(
+    /* optional default value */
+  );
+
+  const App = props => (
+    <ThemeContext.Provider value={{ primaryColor: green }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+
+
+  
