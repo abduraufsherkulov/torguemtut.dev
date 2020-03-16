18f2f59909a27a0bd06c59910b2e62f0d6dd81ab
abduraufsherkulov <abduraufsherkulov@gmail.com>
2020-03-16 18:12:25 +0500

    home carousel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch, Link, withRouter } from 'react-router-dom';
 import ErrorBoundry from './ErrorBoundry';
 import Navigator from "./components/header/Navigator";
@@ -14,19 +14,47 @@ import WishlistVendorContextProvider from "./contexts/WishlistVendorContext";
 import CommentContextProvider from "./contexts/CommentContext";
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-// import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faBlender, faCar, faHome, faGraduationCap, faLaptop, faMobileAlt, faCameraRetro, faTshirt, faCouch, faDraftingCompass, faHardHat, faClipboardCheck, faHandHoldingUsd, faBabyCarriage, faPaw, faGifts, faBook, faPalette, faTableTennis, faHeartbeat, faBinoculars, faArchive, faUsers } from '@fortawesome/free-solid-svg-icons'
 import SoataContextProvider from "./contexts/SoataContext";
 import UserInfoContextProvider from "./contexts/UserInfoContext";
 import BusinessContextProvider from "./contexts/BusinessContext";
 
-library.add(faBlender, faCar, faHome, faGraduationCap, faLaptop, faMobileAlt, faCameraRetro, faTshirt, faCouch, faDraftingCompass, faHardHat, faClipboardCheck, faHandHoldingUsd, faBabyCarriage, faPaw, faGifts, faBook, faPalette, faTableTennis, faHeartbeat, faBinoculars, faArchive, faUsers);
+library.add(fab, faBlender, faCar, faHome, faGraduationCap, faLaptop, faMobileAlt, faCameraRetro, faTshirt, faCouch, faDraftingCompass, faHardHat, faClipboardCheck, faHandHoldingUsd, faBabyCarriage, faPaw, faGifts, faBook, faPalette, faTableTennis, faHeartbeat, faBinoculars, faArchive, faUsers);
 
 if (process.env.NODE_ENV === 'development') {
   import('./sass/styles.scss');
   import("./sass/fonts.scss");
 }
+
 function App() {
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: '656151361204007',
+        cookie: true,                     // Enable cookies to allow the server to access the session.
+        xfbml: true,                     // Parse social plugins on this webpage.
+        version: 'v6.0',           // Use this Graph API version for this call.
+        autoLogAppEvents: true
+      });
+
+
+      // FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
+      //     statusChangeCallback(response);        // Returns the login status.
+      // });
+    };
+
+
+    (function (d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0&appId=656151361204007&autoLogAppEvents=1';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, [])
+
   return (
     <Router keyLength={12}>
       <ErrorBoundry>
--- src/components/home/HomeCarousel.jsx
+++ src/components/home/HomeCarousel.jsx
@@ -2,45 +2,15 @@ import React, { useEffect } from 'react'
 import { Carousel, Row, Col } from 'antd';
 import SliderPng from "../../images/slider.png";
 import UnderCarouselNews from './UnderCarouselNews';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
 function HomeCarousel() {
     function onChange(a, b, c) {
         // console.log(a, b, c);
     }
-    useEffect(() => {
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: '656151361204007',
-                cookie: true,                     // Enable cookies to allow the server to access the session.
-                xfbml: true,                     // Parse social plugins on this webpage.
-                version: 'v6.0'           // Use this Graph API version for this call.
-            });
 
 
-            // FB.getLoginStatus(function (response) {   // Called after the JS SDK has been initialized.
-            //     statusChangeCallback(response);        // Returns the login status.
-            // });
-        };
-
-
-        (function (d, s, id) {                      // Load the SDK asynchronously
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'))
-    }, [])
-
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://telegram.org/js/telegram-widget.js?7";
-        script.setAttribute("data-telegram-post", "ttuzelon/1702");
-        script.setAttribute("data-width", "100%");
-        script.setAttribute("data-userpic", "auto");
-        script.async = true;
-        let tg = document.getElementById('telegram-widget');
-        tg.appendChild(script);
-    }, [])
     return (
         <div id="homecarousel">
             <div className="advertise-main">
@@ -52,10 +22,13 @@ function HomeCarousel() {
                 <UnderCarouselNews />
             </div>
             <div className="fb-part">
-                <div className="fb-group" data-href="https://www.facebook.com/groups/torguem.tut/" data-width="320" data-show-social-context="true" data-show-metadata="false"></div>
+                <div className="fb-group" data-href="https://www.facebook.com/groups/torguem.tut/" data-width="230" data-show-social-context="true" data-show-metadata="false"></div>
                 <span style={{ marginTop: '12px', display: 'block', width: '100%' }}></span>
-                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fwww.tt.uz%2F&tabs=timeline&width=321&height=170&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=292801224405886" width="100%" height="170" style={{ border: 'none', overflow: 'hidden' }} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                <div style={{ width: '321px', background: 'white' }} id="telegram-widget"></div>
+                <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fwww.tt.uz%2F&tabs&width=230&height=214&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=2496083423969818" width="230" height="214" style={{ border: "none", overflow: "hidden" }} scrolling="no" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                <div style={{ background: 'white', paddingTop: '10px', paddingBottom: '10px', display: 'flex', justifyContent: 'space-around' }}>
+                    <a target="_blank" href="https://t.me/ttuzbekistan" ><FontAwesomeIcon style={{ color: '#289ed9' }} size="4x" icon={['fab', 'telegram']} /></a>
+                    <a target="_blank" href="https://www.youtube.com" ><FontAwesomeIcon style={{ color: '#ff0000' }} size="4x" icon={['fab', 'youtube']} /></a>
+                </div>
             </div>
         </div>
     )
